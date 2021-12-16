7f1bb345dfa7dd9989e3f0152d9c44be2eb38ae8
happierpig <zhaoyilong217@sjtu.edu.cn>
2021-12-16 16:32:23 +0800

    Init commit.

--- src/BackEnd/Operand/Immediate.java
+++ src/BackEnd/Operand/Immediate.java
@@ -1,5 +1,8 @@
 package BackEnd.Operand;
 
+/*
+    A class for Immediate and frame-pointer offset
+ */
 public class Immediate extends Operand{
     public int value;
 
--- src/BackEnd/Operand/PhysicalRegister.java
+++ src/BackEnd/Operand/PhysicalRegister.java
@@ -2,12 +2,24 @@ package BackEnd.Operand;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class PhysicalRegister extends Register{
-    public static final ArrayList<String> PhyRegName = new ArrayList(Arrays.asList("zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2", "s0", "s1",
+
+    public static final ArrayList<String> phyRegName = new ArrayList<>(Arrays.asList("zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2", "s0", "s1",
             "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"));
+
+    public static final HashMap<String,PhysicalRegister> phyReg = new HashMap<>(){
+        {PhysicalRegister.phyRegName.forEach(tmp->put(tmp,new PhysicalRegister(tmp)));}
+    };
 
     public PhysicalRegister(String _name) {
         super(_name);
+    }
+
+    public static PhysicalRegister getPhyReg(String name){
+        PhysicalRegister tmp = phyReg.get(name);
+        assert tmp != null;
+        return tmp;
     }
 }
