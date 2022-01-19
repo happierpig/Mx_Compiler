597d96c1fc84672e9aa8f2b6423ff95f9bbf9c80
happierpig <zhaoyilong217@sjtu.edu.cn>
2022-01-19 10:43:26 +0800

    Fix String constant printer

--- src/BackEnd/Operand/GlobalVar.java
+++ src/BackEnd/Operand/GlobalVar.java
@@ -27,9 +27,17 @@ public class GlobalVar extends Operand{
             raw.append('\t').append(".type").append('\t').append(getName()).append(",@object\n");
             raw.append('\t').append(".section").append('\t').append(".rodata\n");
             raw.append(getName()).append(":\n");
-            raw.append('\t').append(".asciz").append('\t').append('\"').append(literal).append('\"').append('\n');
+            raw.append('\t').append(".asciz").append('\t').append('\"').append(format(literal)).append('\"').append('\n');
             raw.append('\t').append(".size").append('\t').append(getName()).append(", ").append(literal.length());
         }
         return raw.toString();
+    }
+
+    private String format(String raw){
+        return raw.replace("\\", "\\\\")
+                .replace("\n", "\\n")
+                .replace("\0", "")
+                .replace("\t", "\\t")
+                .replace("\"", "\\\"");
     }
 }
