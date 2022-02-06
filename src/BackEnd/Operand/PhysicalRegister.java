641e1de32187ae2ff2016eac93a759137c16e371
happierpig <zhaoyilong217@sjtu.edu.cn>
2022-02-06 20:50:19 +0800

    Modify for color register allocation.

--- src/BackEnd/Operand/PhysicalRegister.java
+++ src/BackEnd/Operand/PhysicalRegister.java
@@ -8,22 +8,17 @@ public class PhysicalRegister extends Register{
     public static final ArrayList<String> phyRegName = new ArrayList<>(Arrays.asList("zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2", "s0", "s1",
             "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"));
 
-    public static final ArrayList<PhysicalRegister> callerSaved = new ArrayList<>(){
-        {
-            add(getPhyReg(1));
-            for(int i = 5;i <= 7;++i) add(getPhyReg(i));
-            for(int i = 10;i <= 17;++i) add(getPhyReg(i));
-            for(int i = 28;i <= 31;++i) add(getPhyReg(i));
-        }
-    };
+    public static final ArrayList<Integer> callerSaved = new ArrayList<>(Arrays.asList(
+            5,6,7,
+            10,11,12,13,14,15,16,17,
+            28,29,30,31
+    ));
 
-    public static final ArrayList<PhysicalRegister> calleeSaved = new ArrayList<>(){
-        {
-            add(getPhyReg(2));
-            add(getPhyReg(8));  add(getPhyReg(9));
-            for(int i = 18;i <= 27;++i) add(getPhyReg(i));
-        }
-    };
+    public static final ArrayList<Integer> calleeSaved = new ArrayList<>(Arrays.asList(
+            2,
+            8,9,
+            18,19,20,21,22,23,24,25,26,27
+    ));
 
     public PhysicalRegister(String _name) {
         super(_name);
